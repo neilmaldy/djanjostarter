31623dcc70d05b387d291c4c8d324e42911b24d7
Neil Maldonado <neilmaldy@gmail.com>
2018-05-21 14:03:22 -0400

    Working on file processing

--- ars/views.py
+++ ars/views.py
@@ -6,8 +6,26 @@ from django.urls import reverse_lazy
 from django.conf import settings
 from django.core.files import File
 import os
+import zipfile
 
 from . import models
+
+
+def handle_zip_file(output_file_path, form_cleaned_data):
+    for key in form_cleaned_data:
+        print(key + ' ' + str(form_cleaned_data[key]))
+    with open(output_file_path, 'w') as f:
+        if zipfile.is_zipfile(form_cleaned_data['input_files']):
+            my_zip_file = zipfile.ZipFile(form_cleaned_data['input_files'])
+            for filename in my_zip_file.namelist():
+                with my_zip_file.open(filename) as fp:
+                    text = str(fp.read())
+                    print(text)
+                    f.write(text.upper())
+            # form.instance.output_file.save('file.txt', File(open(os.path.join(settings.MEDIA_ROOT, 'file.txt'))))
+        else:
+            form_cleaned_data['input_files'].open('r')
+            f.write(str(form_cleaned_data['input_files'].read().upper()))
 
 
 class HomePageView(TemplateView):
@@ -85,8 +103,10 @@ class ReportCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        self.request.FILES['input_files'].open('r')
-        with open(os.path.join(settings.MEDIA_ROOT, 'file.txt'), 'w') as f:
-            f.write(str(self.request.FILES['input_files'].read().upper()))
-        form.instance.output_file.save('file.txt', File(open(os.path.join(settings.MEDIA_ROOT, 'file.txt'))))
+        # self.request.FILES['input_files'].open('r')
+        if 'input_files' in form.cleaned_data and form.cleaned_data['input_files']:
+            output_file_path = os.path.join(settings.MEDIA_ROOT, 'file.txt')
+            handle_zip_file(output_file_path, form.cleaned_data)
+            form.instance.output_file.save('file.txt', File(open(output_file_path)))
         return super().form_valid(form)
+
